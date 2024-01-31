77c6b25517e46e235e4d1c1c841300e8cd847bc4
BZV83 <bzvspry@gmail.com>
2024-01-31 10:21:57 -0700

    added passed name to the check winner funciton

--- SupportClass.cs
+++ SupportClass.cs
@@ -20,21 +20,21 @@ namespace tictactoe
             }
         }
 
-        public static bool CheckForWinner(char[,] gameBoard)
+        public static bool CheckForWinner(char[,] gameBoard, string name)
         {
             for (int i = 0; i <= 2; i++)
             {
                 //Check rows to see if not empty and are the same symbol
                 if (gameBoard[i, 0] == gameBoard[i, 1] && gameBoard[i, 1] == gameBoard[i, 2] && gameBoard[i, 0] != ' ')
                 {
-                    Console.WriteLine($"Player {gameBoard[i, 0]} is the winner!"); //confirm that player wins
+                    Console.WriteLine($"Player {name} is the winner!"); //confirm that player wins
                     return true;
                 }
 
                 //Check columns to see if not empty and are the same symbol
                 if (gameBoard[0, i] == gameBoard[1, i] && gameBoard[1, i] == gameBoard[2, i] && gameBoard[0, i] != ' ')
                 {
-                    Console.WriteLine($"Player {gameBoard[0, i]} is the winner!"); //confirm that player wins
+                    Console.WriteLine($"Player {name} is the winner!"); //confirm that player wins
                     return true;
                 }
             }
@@ -42,14 +42,14 @@ namespace tictactoe
             // Check cross to see if not empty and are same symbol
             if (gameBoard[0, 0] == gameBoard[1, 1] && gameBoard[1, 1] == gameBoard[2, 2] && gameBoard[0, 0] != ' ')
             {
-                Console.WriteLine($"Player {gameBoard[0, 0]} is the winner!"); //confirm winner
+                Console.WriteLine($"Player {name} is the winner!"); //confirm winner
                 return true;
             }
 
             //check other cross
             if (gameBoard[0, 2] == gameBoard[1, 1] && gameBoard[1, 1] == gameBoard[2, 0] && gameBoard[0, 2] != ' ')
             {
-                Console.WriteLine($"Player {gameBoard[0, 2]} is the winner!"); //confirm winner
+                Console.WriteLine($"Player {name} is the winner!"); //confirm winner
                 return true;
             }
 
